7625bcf626d0de3c8476c816c52860ef4f6c5333
Ari2123 <97698918+Ari2123@users.noreply.github.com>
2023-08-08 15:31:23 -0700

    Update __init__.py

--- expense_tracker/app/__init__.py
+++ expense_tracker/app/__init__.py
@@ -5,7 +5,10 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expense_tracker.db'
     db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
     from . import routes
     app.register_blueprint(routes.bp)
     return app
-
