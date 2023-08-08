4cfd84307580072e58b4141bfc73df2d6daa2462
Ari2123 <97698918+Ari2123@users.noreply.github.com>
2023-08-08 15:33:23 -0700

    Update routes.py

--- expense_tracker/app/routes.py
+++ expense_tracker/app/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-from .models import Expense, db
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from .models import Expense, db, User
+from werkzeug.security import generate_password_hash
 
 bp = Blueprint('main', __name__)
 
@@ -16,4 +17,22 @@ def add_expense():
     db.session.add(expense)
     db.session.commit()
     return redirect(url_for('main.home'))
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        password_hash = generate_password_hash(password)
+
+        new_user = User(username=username, email=email, password_hash=password_hash)
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Registration successful! Please log in.', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
 
