838e2a110e29e1f4f6058a364354c1a3d1d5adce
Ari2123 <97698918+Ari2123@users.noreply.github.com>
2023-08-08 15:24:00 -0700

    Update routes.py

--- expense_tracker/app/routes.py
+++ expense_tracker/app/routes.py
@@ -1,7 +1,19 @@
-from flask import render_template
-from app import app
+from flask import Blueprint, render_template, request, redirect, url_for
+from .models import Expense, db
 
-@app.route('/')
+bp = Blueprint('main', __name__)
+
+@bp.route('/')
 def home():
-    return render_template('home.html')
+    expenses = Expense.query.all()
+    return render_template('home.html', expenses=expenses)
+
+@bp.route('/add_expense', methods=['POST'])
+def add_expense():
+    description = request.form['description']
+    amount = request.form['amount']
+    expense = Expense(description=description, amount=amount)
+    db.session.add(expense)
+    db.session.commit()
+    return redirect(url_for('main.home'))
 
