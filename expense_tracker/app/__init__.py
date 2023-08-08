9cdc3ff78a32b2b324b11e2164e29f6fff3a5bce
Ari2123 <97698918+Ari2123@users.noreply.github.com>
2023-08-08 15:37:32 -0700

    Update __init__.py

--- expense_tracker/app/__init__.py
+++ expense_tracker/app/__init__.py
@@ -3,7 +3,7 @@ from .models import db
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your-secret-key-here'
+    app.config['SECRET_KEY'] = '212123'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expense_tracker.db'
     db.init_app(app)
 
