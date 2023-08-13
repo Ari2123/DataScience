e66b5c74a1b56d5e5eac8b72070c17a2e7a8a081
Ari2123 <97698918+Ari2123@users.noreply.github.com>
2023-08-12 19:39:25 -0700

    Update models.py

--- expense_tracker/app/models.py
+++ expense_tracker/app/models.py
@@ -1,9 +1,12 @@
-from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    """User model for storing user details and their hashed passwords."""
+    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
@@ -12,24 +15,29 @@ class User(db.Model):
     expenses = db.relationship('Expense', backref='user', lazy=True)
 
     def set_password(self, password):
+        """Hash and set the password."""
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
+        """Check if the hashed password matches the current hashed password."""
         return check_password_hash(self.password_hash, password)
 
 class Category(db.Model):
+    """Category model for classifying expenses."""
+    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
 
     expenses = db.relationship('Expense', backref='category', lazy=True)
 
 class Expense(db.Model):
+    """Expense model for storing expense details."""
+    
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(100), nullable=False)
     amount = db.Column(db.Float, nullable=False)
-    date = db.Column(db.DateTime, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)  # Set the default date
 
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=True)
-
 
