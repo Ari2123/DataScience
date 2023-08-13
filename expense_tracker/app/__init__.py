920baa25ffa62778664c334e8b8c2321bff10ce2
Ari2123 <97698918+Ari2123@users.noreply.github.com>
2023-08-12 19:35:46 -0700

    Update __init__.py

--- expense_tracker/app/__init__.py
+++ expense_tracker/app/__init__.py
@@ -1,15 +1,25 @@
+import os
 from flask import Flask
 from .models import db
 
+# Load environment variables from the .env file
+from dotenv import load_dotenv
+load_dotenv()
+
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = '212123'
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'a_default_fallback_key'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expense_tracker.db'
+    
     db.init_app(app)
 
-    with app.app_context():
+    @app.cli.command("initdb")
+    def initdb_command():
+        """Initialize the database."""
         db.create_all()
+        print("Initialized the database.")
 
     from . import routes
     app.register_blueprint(routes.bp)
+
     return app
