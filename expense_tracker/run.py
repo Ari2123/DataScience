633e3f472be3fb76182a2f0ea5e231aed9005f91
Ari2123 <97698918+Ari2123@users.noreply.github.com>
2023-08-12 20:04:22 -0700

    Update run.py

--- expense_tracker/run.py
+++ expense_tracker/run.py
@@ -2,5 +2,8 @@ from app import create_app
 
 app = create_app()
 
+app.config['ENV'] = 'development'
+app.config['DEBUG'] = True
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
